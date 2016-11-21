a1ee79854dcbc86be27a9999760e2e2bcf49091f
bleader <bleader@ratonland.org>
2016-11-21 23:05:05 +0100

    deep sleep and debug option
    - rewrite to actually use deep sleep
    - add a DEBUG_MODE that allows to disable all prints, that gets active
      time from 4150ms to 4070ms, saving about 80ms for each wake.

--- src/bme.cpp
+++ src/bme.cpp
@@ -3,29 +3,36 @@
 #include <SPI.h>
 #include <BME280I2C.h>
 
-#define FW_NAME		"raton-bme"
-#define FW_VERSION	"0.0.3"
+/* comment this out to remove almost all prints
+ * this saves an average of 80ms on a roughly 4s uptime between deep sleeps */
+#define DEBUG_MODE 1
+
+#define FW_NAME		"raton-bme-deepsleep"
+#define FW_VERSION	"0.0.4"
 
 /* Magic sequence for Autodetectable Binary Upload */
 const char *__FLAGGED_FW_NAME = "\xbf\x84\xe4\x13\x54" FW_NAME "\x93\x44\x6b\xa7\x75";
 const char *__FLAGGED_FW_VERSION = "\x6a\x3f\x3e\x0e\xe1" FW_VERSION "\xb0\x30\x48\xd4\x1a";
 /* End of magic sequence for Autodetectable Binary Upload */
 
-#define PUB_INTERVAL  60
 
 HomieNode temperatureNode("temperature", "temperature");
 HomieNode humidityNode("humidity", "humidity");
 HomieNode pressureNode("pressure", "pressure");
+HomieNode batteryNode("battery", "battery");
 
 BME280I2C bme;
 #define PRESSURE_OFFSET 16
 
-unsigned long lastPublish = 0;
+ADC_MODE(ADC_VCC);
+bool published = false;
+uint32_t start;
 
 void setupHandler() {
 	temperatureNode.setProperty("unit").send("c");
 	humidityNode.setProperty("unit").send("%");
 	pressureNode.setProperty("unit").send("hPa");
+	batteryNode.setProperty("unit").send("V");
 
 	/* 4 and 5 are the gpio numbers */
 	if (!bme.begin(4,5))
@@ -33,29 +40,53 @@ void setupHandler() {
 }
 
 void loopHandler() {
-	if (millis() - lastPublish >= PUB_INTERVAL * 1000UL) {
-		float t, h, p;
+	/* make sure not to send anything until mqtt is connected */
+	if (Homie.isConnected() && !published) {
+		float t, h, p, v;
 
 		bme.read(p, t, h, true, 1); /* true for metric, 1 for hPa */
 		p += PRESSURE_OFFSET;
+		v = ESP.getVcc() / 1000.0f;
 
-		if (!isnan(t) &&
-		    temperatureNode.setProperty("degrees").send(String(t))) {
-			lastPublish = millis();
-		}
-		if (!isnan(h) &&
-		    humidityNode.setProperty("relative").send(String(h))) {
-			lastPublish = millis();
-		}
-		if (!isnan(p) &&
-		    pressureNode.setProperty("pressure").send(String(p))) {
-			lastPublish = millis();
-		}
+#ifdef DEBUG_MODE
+		Serial << "t = " << t << "°C p = " << p << "hPa / h = " << h << " % /  v = " << v  << "V" << endl;
+#endif
+		if (!isnan(t))
+		    temperatureNode.setProperty("degrees").send(String(t));
+		if (!isnan(h))
+		    humidityNode.setProperty("relative").send(String(h));
+		if (!isnan(p))
+		    pressureNode.setProperty("pressure").send(String(p));
+		if (!isnan(v))
+			batteryNode.setProperty("battery").send(String(v));
+		published = true;
+		Homie.prepareToSleep();
 	}
 }
 
+void onHomieEvent(const HomieEvent& event) {
+	switch(event.type) {
+		case HomieEventType::READY_TO_SLEEP:
+#ifdef DEBUG_MODE
+			Serial << "Ready to sleep" << endl;
+#endif
+			Serial << millis() - start << endl;
+			ESP.deepSleep(10 * 1000000); /* 10min */
+			break;
+ 	}
+ }
+
 void setup() {
+	start = millis();
+	published = false;
+
+#ifdef DEBUG_MODE
 	Serial.begin(115200);
+	Serial.println();
+	Serial.println();
+#else
+	Homie.disableLogging();
+#endif
 
 	Homie_setFirmware(FW_NAME, FW_VERSION);
 
@@ -63,6 +94,7 @@ void setup() {
 	Homie.setLoopFunction(loopHandler);
 
 	Homie.disableLedFeedback(); 
+	Homie.onEvent(onHomieEvent);
 	Homie.setup();
 }
 
